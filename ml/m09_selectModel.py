cce2d7b6a2c30078493580d9e206a019b2995548
CHAEUN PARK <codjs0910@naver.com>
2019-10-13 09:40:03 +0900

    gitignore update

---
+++ ml/m09_selectModel.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import warnings
+from sklearn.utils.testing import all_estimators
+
